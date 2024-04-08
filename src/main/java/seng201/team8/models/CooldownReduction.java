44ffa35f825fc7a73922bf77675854a667adb1ef
Brenden Lee <dhl28@uclive.ac.nz>
2024-04-08 12:08:17 +1200

    Created base CooldownReduction Class

---
+++ src/main/java/seng201/team8/models/CooldownReduction.java
@@ -0,0 +1,19 @@
+package seng201.team8.models;
+
+import java.util.ArrayList;
+
+public class CooldownReduction implements Effect{
+
+    private int reductionAmount;
+
+    public CooldownReduction(int reductionAmount){
+        this.reductionAmount = reductionAmount;
+    }
+
+    @Override
+    public void affects(ArrayList<Tower> towers) {
+        for(Tower tower: towers){
+            tower.getTowerStats().setCooldown(tower.getTowerStats().getCooldown() - reductionAmount);
+        }
+    }
+}
