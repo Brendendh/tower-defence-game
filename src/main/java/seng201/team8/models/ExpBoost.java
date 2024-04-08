df11d8770304818f508f19883fd944fa5e650e40
Brenden Lee <dhl28@uclive.ac.nz>
2024-04-08 12:04:49 +1200

    Created base ExpBoost Class

---
+++ src/main/java/seng201/team8/models/ExpBoost.java
@@ -0,0 +1,23 @@
+package seng201.team8.models;
+
+import java.util.ArrayList;
+
+public class ExpBoost implements Effect{
+    private int boostAmount;
+
+    public ExpBoost(int boostAmount){
+        this.boostAmount = boostAmount;
+    }
+
+    @Override
+    public void affects(ArrayList<Tower> towers) {
+        for(Tower tower: towers){
+            tower.setExperiencePoints(tower.getExperiencePoints() + boostAmount);
+        }
+    }
+
+    public int getBoostAmount(){
+        return boostAmount;
+    }
+
+}
