e9923ad3c4bec3c3d91b9d0b76f2bca2d94c3f0d
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-04-12 20:36:10 +1200

    added updateValues() and made so the constructor properly update values of the effects and buying price based off rarity

--- src/main/java/seng201/team8/models/Upgrade.java
+++ src/main/java/seng201/team8/models/Upgrade.java
@@ -5,12 +5,26 @@ public class Upgrade extends Item{
     private int maximumTargets;
 
     public Upgrade(Effect effect, Rarity rarity, int buyingPrice, int maximumTargets){
-        super(buyingPrice, 0, rarity);
+        super(buyingPrice * rarity.rarityStatMultiplier, 0, rarity);
         this.maximumTargets = maximumTargets;
         this.effect = effect;
+        updateValues(this.effect, rarity);
     }
 
     public Effect getEffect() {
         return effect;
     }
+    public int getMaximumTargets(){return maximumTargets;}
+    private void updateValues(Effect effect, Rarity rarity){
+        if (effect.getClass() == ExpBoost.class){
+            ((ExpBoost) effect).setBoostAmount(((ExpBoost) effect).getBoostAmount() * rarity.rarityStatMultiplier);
+        }
+        if (effect.getClass() == CooldownReduction.class){
+            ((CooldownReduction) effect).setReductionAmount(((CooldownReduction) effect).getReductionAmount() * rarity.rarityStatMultiplier);
+        }
+        if (effect.getClass() == ResourceAmountBoost.class){
+            ((ResourceAmountBoost) effect).setBoostAmount(((ResourceAmountBoost) effect).getBoostAmount() * rarity.rarityStatMultiplier);
+        }
+    }
+
 }
