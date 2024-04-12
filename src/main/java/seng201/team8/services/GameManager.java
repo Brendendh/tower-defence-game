df42f14fe326518c0b2457a4778016b1d886cf96
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-04-12 19:54:07 +1200

    added rarity array and their getters to GameManager

--- src/main/java/seng201/team8/services/GameManager.java
+++ src/main/java/seng201/team8/services/GameManager.java
@@ -4,21 +4,38 @@ import seng201.team8.models.*;
 
 public class GameManager {
     private GameData gameData;
-    final Tower[] defaultTowers = new Tower[]{new Tower("", new TowerStats(10,"Coal",5),10, Rarity.COMMON),
+    final private Tower[] defaultTowers = new Tower[]{
+            new Tower("", new TowerStats(10,"Coal",5),10, Rarity.COMMON),
             new Tower("", new TowerStats(10,"Corn",5),10, Rarity.COMMON),
             new Tower("", new TowerStats(10,"Stone",5),10, Rarity.COMMON),
             new Tower("", new TowerStats(10,"Steel",5),10, Rarity.COMMON),
             new Tower("", new TowerStats(10,"Diamonds",5),10, Rarity.COMMON)};
 
-    final Upgrade[] defaultUpgrades = new Upgrade[]{new Upgrade(new ExpBoost(10), Rarity.COMMON,10,1),
+    final private Upgrade[] defaultUpgrades = new Upgrade[]{
+            new Upgrade(new ExpBoost(10), Rarity.COMMON,10,1),
             new Upgrade(new CooldownReduction(10), Rarity.COMMON,10,1),
             new Upgrade(new ResourceAmountBoost(10), Rarity.COMMON,10,1)};
 
+    final private Rarity[] earlyGameRarity = new Rarity[]{Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.RARE,Rarity.RARE, Rarity.RARE};
+    final private Rarity[] midGameRarity = new Rarity[]{Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.COMMON,Rarity.RARE,Rarity.RARE,Rarity.RARE,Rarity.EPIC,Rarity.EPIC};
+    final private Rarity[] lateGameParty = new Rarity[]{Rarity.COMMON, Rarity.COMMON,Rarity.COMMON,Rarity.RARE,Rarity.RARE,Rarity.RARE,Rarity.RARE,Rarity.EPIC,Rarity.EPIC,Rarity.EPIC};
     public Tower[] getDefaultTowers() {
         return defaultTowers;
     }
 
     public Upgrade[] getDefaultUpgrades() {
         return defaultUpgrades;
+    }
+
+    public Rarity[] getEarlyGameRarity() {
+        return earlyGameRarity;
+    }
+
+    public Rarity[] getMidGameRarity() {
+        return midGameRarity;
+    }
+
+    public Rarity[] getLateGameParty() {
+        return lateGameParty;
     }
 }
