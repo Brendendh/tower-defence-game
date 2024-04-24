d8099dbd831740c6bbe61ae96a2dc2fa633b11a4
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-04-24 15:46:30 +1200

    added clone function to Upgrade

--- src/main/java/seng201/team8/models/Upgrade.java
+++ src/main/java/seng201/team8/models/Upgrade.java
@@ -1,6 +1,6 @@
 package seng201.team8.models;
 
-public class Upgrade extends Item{
+public class Upgrade extends Item implements Cloneable{
     private Effect effect;
     private int maximumTargets;
 
@@ -25,6 +25,41 @@ public class Upgrade extends Item{
         if (effect.getClass() == ResourceAmountBoost.class){
             ((ResourceAmountBoost) effect).setBoostAmount(((ResourceAmountBoost) effect).getBoostAmount() * rarity.rarityStatMultiplier);
         }
+    }
+
+    public int getBoostAmount(){
+        int amount = 0;
+        if (effect.getClass() == ExpBoost.class){
+            amount = ((ExpBoost) effect).getBoostAmount();
+        }
+        if (effect.getClass() == CooldownReduction.class){
+            amount =((CooldownReduction) effect).getReductionAmount();
+        }
+        if (effect.getClass() == ResourceAmountBoost.class){
+            amount =((ResourceAmountBoost) effect).getBoostAmount();
+        }
+        return amount;
+    }
+
+    @Override
+    public Upgrade clone(){
+        Upgrade clonedUpgrade;
+        try{
+            clonedUpgrade = (Upgrade) super.clone();
+        }
+        catch (CloneNotSupportedException e){
+            clonedUpgrade = new Upgrade(this.getEffect(),this.getRarity(), this.getBuyingPrice(), this.getMaximumTargets());
+        }
+        if (clonedUpgrade.getEffect().getClass() == ExpBoost.class){
+            clonedUpgrade.effect = ((ExpBoost) this.effect).clone();
+        }
+        if (clonedUpgrade.getEffect().getClass() == CooldownReduction.class){
+            clonedUpgrade.effect = ((CooldownReduction) this.effect).clone();
+        }
+        if (clonedUpgrade.getEffect().getClass() == ResourceAmountBoost.class){
+            clonedUpgrade.effect = ((ResourceAmountBoost) this.effect).clone();
+        }
+        return clonedUpgrade;
     }
 
 }
