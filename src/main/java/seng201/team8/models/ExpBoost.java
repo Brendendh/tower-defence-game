42d99944929799f5da6311dc12d3cadfe2020943
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-04-24 15:46:06 +1200

    added clone function to ExpBoost

--- src/main/java/seng201/team8/models/ExpBoost.java
+++ src/main/java/seng201/team8/models/ExpBoost.java
@@ -2,7 +2,7 @@ package seng201.team8.models;
 
 import java.util.ArrayList;
 
-public class ExpBoost implements Effect{
+public class ExpBoost implements Effect, Cloneable{
     private int boostAmount;
 
     public ExpBoost(int boostAmount){this.boostAmount = boostAmount;}
@@ -11,6 +11,16 @@ public class ExpBoost implements Effect{
     public void affects(ArrayList<Tower> towers) {
         for(Tower tower: towers){
             tower.setExperiencePoints(tower.getExperiencePoints() + boostAmount);
+        }
+    }
+
+    @Override
+    public ExpBoost clone(){
+        try{
+            return (ExpBoost) super.clone();
+        }
+        catch (CloneNotSupportedException e){
+            return new ExpBoost(this.getBoostAmount());
         }
     }
 
