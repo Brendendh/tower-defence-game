cd44cf069a887c28d5e2e7a5e227a06aace2c0f8
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-05-02 14:10:33 +1200

    updated affects method to use addExp

--- src/test/java/seng201/team8/unittests/services/TowerStatsManagerTest.java
+++ src/test/java/seng201/team8/unittests/services/TowerStatsManagerTest.java
@@ -1,14 +1,11 @@
 package seng201.team8.unittests.services;
 
-import seng201.team8.models.InventoryData;
-import seng201.team8.models.Rarity;
-import seng201.team8.models.Tower;
-import seng201.team8.models.TowerStats;
+import seng201.team8.models.*;
 import seng201.team8.services.TowerStatsManager;
 
 public class TowerStatsManagerTest {
     public static void main(String[] args) {
-        Tower[] testTowers = new Tower[]{new Tower("Starting Tower", new TowerStats(10, "Coal", 10), 10, Rarity.COMMON), new Tower("Seoncd Tower", new TowerStats(10, "Coal", 10), 10, Rarity.COMMON), null, null, null};
+        Tower[] testTowers = new Tower[]{new Tower("Starting Tower", new TowerStats(10, Resource.CORN, 10), 10, Rarity.COMMON), new Tower("Seoncd Tower", new TowerStats(10, Resource.WOOD, 10), 10, Rarity.COMMON), null, null, null};
         InventoryData inventoryData = new InventoryData();
         inventoryData.setMainTowers(testTowers);
         TowerStatsManager test1 = new TowerStatsManager(inventoryData);
