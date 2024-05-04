a6c23d71c3067779192503c3de2642e9846adc86
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-05-04 16:54:37 +1200

    added Rarity Color Attribute to Rarity Enum

--- src/main/java/seng201/team8/models/Rarity.java
+++ src/main/java/seng201/team8/models/Rarity.java
@@ -1,10 +1,16 @@
 package seng201.team8.models;
 
+import javafx.scene.paint.Color;
+
 public enum Rarity {
-    COMMON(1), RARE(2), EPIC(3);
+    COMMON(1, Color.color(0,0,0)),
+    RARE(2, Color.color(255,165,0)),
+    EPIC(3, Color.color(160,32,240));
 
     final int rarityStatMultiplier;
-    Rarity(int rarityStatMultiplier){
+    final Color rarityTextColor;
+    Rarity(int rarityStatMultiplier, Color rarityTextColor){
         this.rarityStatMultiplier = rarityStatMultiplier;
+        this.rarityTextColor = rarityTextColor;
     }
 }
