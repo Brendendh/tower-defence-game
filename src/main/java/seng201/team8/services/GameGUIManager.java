846592fa1854900d4f77dca02fd94be78c21b4e8
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-05-09 18:30:35 +1200

    added Round Selector Screen

--- src/main/java/seng201/team8/services/GameGUIManager.java
+++ src/main/java/seng201/team8/services/GameGUIManager.java
@@ -1,9 +1,6 @@
 package seng201.team8.services;
 
-import seng201.team8.gui.GameMenuController;
-import seng201.team8.gui.GameSetupController;
-import seng201.team8.gui.GameStartController;
-import seng201.team8.gui.ShopScreenController;
+import seng201.team8.gui.*;
 import seng201.team8.models.Scene;
 
 import java.util.HashMap;
@@ -30,6 +27,7 @@ public class GameGUIManager {
             scenes.put("Game Setup", new Scene("Game Setup", "GameSetupScreen.fxml", GameSetupController.class.getConstructor(GameManager.class)));
             scenes.put("Game Menu", new Scene("Game Menu", "GameMenuScreen.fxml", GameMenuController.class.getConstructor(GameManager.class)));
             scenes.put("Shop Screen", new Scene("Shop Screen", "ShopScreen.fxml", ShopScreenController.class.getConstructor(GameManager.class)));
+            scenes.put("Round Selector", new Scene("Round Selector", "RoundSelectorScreen.fxml",RoundSelectorScreenController.class.getConstructor(GameManager.class)));
         } catch (NoSuchMethodException e) {
             throw new RuntimeException(e);
         }
