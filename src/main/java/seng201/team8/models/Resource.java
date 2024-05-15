05326471648903be041c41dd18fab2d0736f2b4e
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-05-15 16:30:51 +1200

    added a getter for resource value

--- src/main/java/seng201/team8/models/Resource.java
+++ src/main/java/seng201/team8/models/Resource.java
@@ -3,6 +3,11 @@ package seng201.team8.models;
 public enum Resource {
     CORN(1), WOOD(4), IRON(10);
 
-    final int resourceValue;
+    final private int resourceValue;
     Resource(int resourceValue) {this.resourceValue = resourceValue;}
+
+    public int getResourceValue(){
+        return resourceValue;
+    }
+
 }
