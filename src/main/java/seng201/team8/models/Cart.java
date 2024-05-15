06f8db4a03d5770e8eebab0d937ee50b324a22e2
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-05-15 16:49:27 +1200

    added documentation to Cart model class

--- src/main/java/seng201/team8/models/Cart.java
+++ src/main/java/seng201/team8/models/Cart.java
@@ -1,11 +1,45 @@
 package seng201.team8.models;
 
+/**
+ * The model for the Cart objects. Used to create the Round class in the form of a list of carts.
+ * Once a cart travels a certain amount of distance without being filled up completely, the
+ * round is considered lost.
+ */
+
 public class Cart {
+    /**
+     * An Integer value that determines the cart's capacity.
+     * The cart is considered filled once its amount reaches or exceeds the targetAmount.
+     */
     private int targetAmount;
+    /**
+     * A Resource value that determines which type of resource the cart will accept.
+     * Only towers of the same resource type fill up the cart of said resource type.
+     */
     private Resource resourceType;
+    /**
+     * An Integer value representing the cart's speed. Determines how much distance the cart
+     * traverses each turn.
+     */
     private int speed;
+    /**
+     * An Integer value representing the cart's current resource amount. Once it reaches/exceeds
+     * targetAmount, the cart is considered filled
+     */
     private int amount;
+    /**
+     * An Integer value representing the cart's current distance from the starting point.
+     * The starting distance of a cart is always 0 at the first turn. Once the distance is equal or larger
+     * than the Round's distanceAllowed and the cart is not filled, the round is considered lost.
+     */
     private int distance;
+
+    /**
+     * The constructor for a cart.
+     * @param targetAmount the Integer number of resources needed to fill up the cart.
+     * @param resourceType the Resource type that the cart accepts
+     * @param speed the Integer value of how much the cart's distance increases per turn
+     */
     public Cart(int targetAmount, Resource resourceType, int speed){
         this.targetAmount = targetAmount;
         this.resourceType = resourceType;
@@ -13,38 +47,75 @@ public class Cart {
         this.amount = 0;
         this.distance = 0;
     }
+
+    /**
+     * Adds an Integer value to the cart's current resource amount
+     * @param amountToAdd An Integer amount to be added to the cart's current amount value
+     */
     public void increaseAmount(int amountToAdd){
         amount += amountToAdd;
     }
 
+    /**
+     * Increases the cart's distance travelled by an integer amount
+     * @param distanceToAdd An Integer amount to be added to the cart's distance travelled
+     */
     public void increaseDistance(int distanceToAdd){
         distance += distanceToAdd;
     }
 
+    /**
+     * Returns the cart's target amount of resources
+     * @return Integer
+     */
     public int getTargetAmount() {
         return targetAmount;
     }
 
+    /**
+     * returns the Resource type of the cart
+     * @return Resource Enumerator
+     */
     public Resource getResourceType() {
         return resourceType;
     }
 
+    /**
+     * returns the speed of the cart
+     * @return Integer
+     */
     public int getSpeed() {
         return speed;
     }
 
+    /**
+     * returns the current distance of the cart
+     * @return Integer
+     */
     public int getDistance() {
         return distance;
     }
 
+    /**
+     * returns the current resource amount of the cart
+     * @return Integer
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Sets the resourceAmount of the cart to the specified value
+     * @param amount an Integer value
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * Sets the current distance of the cart to the specified value
+     * @param distance an Integer value
+     */
     public void setDistance(int distance) {
         this.distance = distance;
     }
