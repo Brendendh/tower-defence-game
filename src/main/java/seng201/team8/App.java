b4ed8a12b693504f0b4a1ae2650f70e0c36bcd22
Brenden Lee <dhl28@uclive.ac.nz>
2024-05-19 23:53:38 +1200

    Written documentation for App

--- src/main/java/seng201/team8/App.java
+++ src/main/java/seng201/team8/App.java
@@ -2,7 +2,15 @@ package seng201.team8;
 
 import seng201.team8.gui.FXWindow;
 
+/**
+ * Default entry point class
+ */
 public class App {
+
+    /**
+     * Entry point which runs the JavaFX application.
+     * @param args program arguments from command line
+     */
     public static void main(String[] args) {
         FXWindow.launchWrapper(args);
     }
