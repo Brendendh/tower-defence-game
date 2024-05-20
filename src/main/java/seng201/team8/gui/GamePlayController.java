18b05433ecf084c888bca54e54aacdf3877e1a97
Brenden Lee <dhl28@uclive.ac.nz>
2024-05-20 13:23:58 +1200

    Formatted GamePlayController

--- src/main/java/seng201/team8/gui/GamePlayController.java
+++ src/main/java/seng201/team8/gui/GamePlayController.java
@@ -2,23 +2,17 @@ package seng201.team8.gui;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.util.Duration;
-import seng201.team8.models.Cart;
-import seng201.team8.models.Resource;
 import seng201.team8.models.Round;
 import seng201.team8.services.GameManager;
 import seng201.team8.services.RoundEvaluationService;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,9 +25,9 @@ public class GamePlayController {
 
     private GridPane gamePane;
 
-    private GameManager gameManager;
-    private Round round;
-    private RoundEvaluationService roundEvaluationService;
+    private final GameManager gameManager;
+    private final Round round;
+    private final RoundEvaluationService roundEvaluationService;
 
     private List<Label> cartLabels;
     private List<Label> resourceLabels;
@@ -49,11 +43,77 @@ public class GamePlayController {
 
     public void initialize(){
         gamePane = new GridPane();
+        gamePane.getStyleClass().add("grid");
         gamePane.prefWidthProperty().bind(mainLayout.widthProperty());
         cartLabels = new ArrayList<Label>();
         resourceLabels = new ArrayList<Label>();
-        gamePane.setGridLinesVisible(true);
 
+        createTableConstraints();
+        createGameTable();
+
+        createCartLabels();
+        createResourceLabels();
+
+        mainLayout.getStylesheets().add("/css/GridBorders.css");
+
+        mainLayout.add(gamePane, 1, 0);
+
+        playAnimation();
+    }
+
+    private void playAnimation() {
+        Timeline timeline = new Timeline();
+
+        KeyFrame resourceKeyFrame = getResourceKeyFrame(timeline);
+        KeyFrame cartKeyFrame = getCartKeyFrame(timeline);
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add(resourceKeyFrame);
+        timeline.getKeyFrames().add(cartKeyFrame);
+        timeline.play();
+    }
+
+    private KeyFrame getResourceKeyFrame(Timeline timeline) {
+        return new KeyFrame(
+                Duration.seconds(0.1),
+                event -> {
+                    if(roundEvaluationService.didCartReach()){
+                        timeline.stop();
+                        finishGame(false);
+                    }
+                    roundEvaluationService.incrementCounter();
+                    roundEvaluationService.produceResources();
+                    updateResourcePane();
+                }
+        );
+    }
+
+    private KeyFrame getCartKeyFrame(Timeline timeline) {
+        return new KeyFrame(
+                Duration.seconds(0.5),
+                event -> {
+                    roundEvaluationService.fillCarts();
+                    if (roundEvaluationService.areAllCartsFull()) {
+                        timeline.stop();
+                        finishGame(true);
+                    }
+                    roundEvaluationService.advanceCarts();
+                    updateGamePane();
+                    updateResourcePane();
+                }
+        );
+    }
+
+    private void createResourceLabels() {
+        for(int i = 0; i < gameManager.getDefaultResources().length; i++){
+            //Change to toString
+            Label tempLabel = new Label(gameManager.getDefaultResources()[i] + " : 0");
+            resourceLabels.add(tempLabel);
+            resourcePane.add(tempLabel, 0, i);
+        }
+    }
+
+    private void createTableConstraints() {
         numCols = round.getDistanceAllowed();
         numRows = round.getCartNumber();
         for (int i = 0; i < numCols; i++) {
@@ -66,63 +126,6 @@ public class GamePlayController {
             rowConst.setPercentHeight(100.0 / numRows);
             gamePane.getRowConstraints().add(rowConst);
         }
-        createTable();
-        gamePane.getStyleClass().add("grid");
-
-        for (int i = 0; i < round.getCartNumber(); i++){
-            Label tempLabel = new Label("cart" + i);
-            tempLabel.setWrapText(true);
-            cartLabels.add(tempLabel);
-            gamePane.add(tempLabel, 0, i);
-        }
-        for(int i = 0; i < gameManager.getDefaultResources().length; i++){
-            //Change to toString
-            Label tempLabel = new Label(gameManager.getDefaultResources()[i] + " : 0");
-            resourceLabels.add(tempLabel);
-            resourcePane.add(tempLabel, 0, i);
-        }
-
-        mainLayout.getStylesheets().add("/css/GridBorders.css");
-
-        mainLayout.add(gamePane, 1, 0);
-
-        Timeline timeline = new Timeline();
-        KeyFrame resourceKeyFrame = new KeyFrame(
-                Duration.seconds(0.1),
-                event -> {
-                    if(roundEvaluationService.didCartReach()){
-                        timeline.stop();
-                        finishGame(false);
-                    }
-                    roundEvaluationService.incrementCounter();
-                    roundEvaluationService.produceResources();
-                    clearResourcePane();
-                    updateResourceTable();
-                }
-        );
-        KeyFrame cartKeyFrame = new KeyFrame(
-                Duration.seconds(0.5),
-                event -> {
-                    roundEvaluationService.fillCarts();
-                    if (roundEvaluationService.areAllCartsFull()) {
-                        timeline.stop();
-                        finishGame(true);
-                    }
-                    roundEvaluationService.advanceCarts();
-                    clearGamePane();
-                    updateTable();
-                    clearResourcePane();
-                    updateResourceTable();
-                }
-        );
-        timeline.setCycleCount(Timeline.INDEFINITE);
-        timeline.getKeyFrames().add(resourceKeyFrame);
-        timeline.getKeyFrames().add(cartKeyFrame);
-        timeline.play();
-    }
-
-    private void checkGameCondition() {
-
     }
 
     private void finishGame(boolean gameResult) {
@@ -130,16 +133,26 @@ public class GamePlayController {
         gameManager.getGameGUIManager().launchScreen("Round Evaluation");
     }
 
+    private void updateGamePane(){
+        clearGamePane();
+        createGameTable();
+        updateGameTable();
+    }
+
+    private void updateResourcePane(){
+        clearResourcePane();
+        updateResourceTable();
+    }
+
     private void clearGamePane(){
         gamePane.getChildren().clear();
-        createTable();
     }
 
     private void clearResourcePane(){
         resourcePane.getChildren().clear();
     }
 
-    private void createTable() {
+    private void createGameTable() {
         for (int i = 0; i < numCols; i++) {
             for (int j = 0; j < numRows; j++) {
                 Pane pane = new Pane();
@@ -149,7 +162,7 @@ public class GamePlayController {
         }
     }
 
-    private void updateTable(){
+    private void updateGameTable(){
         for(int i = 0; i < round.getCartNumber(); i++){
             cartLabels.get(i).setText(roundEvaluationService.getCarts()[i].toString());
             gamePane.add(cartLabels.get(i), roundEvaluationService.getCarts()[i].getDistance() , i);
@@ -160,6 +173,15 @@ public class GamePlayController {
         for(int i = 0; i < roundEvaluationService.getResourcesProduced().size(); i++){
             resourceLabels.get(i).setText(gameManager.getDefaultResources()[i] + " : " +roundEvaluationService.getResourcesProduced().get(gameManager.getDefaultResources()[i]).toString());
             resourcePane.add(resourceLabels.get(i), 0, i);
+        }
+    }
+
+    private void createCartLabels(){
+        for (int i = 0; i < round.getCartNumber(); i++){
+            Label tempLabel = new Label("cart" + i);
+            tempLabel.setWrapText(true);
+            cartLabels.add(tempLabel);
+            gamePane.add(tempLabel, 0, i);
         }
     }
 
