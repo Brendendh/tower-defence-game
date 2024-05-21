6078ed27dbf14c246cf73da25f1adda51925b477
Brenden Lee <dhl28@uclive.ac.nz>
2024-05-21 12:25:16 +1200

    Created function to create Inventory manager

--- src/main/java/seng201/team8/services/GameSetupService.java
+++ src/main/java/seng201/team8/services/GameSetupService.java
@@ -51,4 +51,8 @@ public class GameSetupService {
         inventoryData.setMainTowers(mainTowers);
         return inventoryData;
     }
+
+    public InventoryManager createInventoryManager(InventoryData inventoryData){
+        return new InventoryManager(inventoryData);
+    }
 }
