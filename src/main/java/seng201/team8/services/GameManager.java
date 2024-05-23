47e4fcd2538aef37505af43068b19a4353eafc2e
Wen Sheng Thong <wst44@uclive.ac.nz>
2024-05-23 23:01:37 +1200

    javadoc for GameManager

--- src/main/java/seng201/team8/services/GameManager.java
+++ src/main/java/seng201/team8/services/GameManager.java
@@ -51,6 +51,8 @@ public class GameManager {
      */
     private final RarityData rarityData = new RarityData();
     /**
+     * The {@link GameGUIManager} for the game. Used by controllers
+     * to launch different {@link Scene}s.
      *
      */
     private GameGUIManager gameGUIManager;
@@ -64,8 +66,36 @@ public class GameManager {
      * @see seng201.team8.gui.GameMenuController
      */
     private ArrayList<Resource> roundResourceDisplay;
+    /**
+     * Boolean value that represents if the current round is won or not.
+     * Used by round evaluation service and controller to decide which screen to
+     * launch to.
+     *
+     * @see RoundEvaluationService
+     * @see seng201.team8.gui.RoundEvaluationController
+     */
     private boolean roundWon = false;
+    /**
+     * An array of {@link Resource} containing the possible resource types.
+     *
+     *<p></p>
+     * Used by RoundSelectorService and RandomEventsService as a pool of possible
+     * resource types to choose from.
+     *
+     * @see RoundSelectorService
+     * @see RandomEventsService
+     */
     final private Resource[] defaultResources = new Resource[]{Resource.CORN, Resource.WOOD, Resource.IRON};
+
+    /**
+     * An Array of the 5 different types of {@link Tower}s available in the game.
+     *<p></p>
+     * Accessed by the ShopManager and GameSetupService when generating towers for the players.
+     * Their rarities are modified when generated.
+     *
+     * @see ShopManager
+     * @see GameSetupService
+     */
     final private Tower[] defaultTowers = new Tower[]{
             new Tower("Corn Farm", new TowerStats(10, Resource.CORN, 1 ), 10, Rarity.COMMON),
             new Tower("Corn Field", new TowerStats(35, Resource.CORN, 3), 15, Rarity.COMMON),
@@ -73,6 +103,14 @@ public class GameManager {
             new Tower("Forest", new TowerStats(30, Resource.WOOD, 3), 15, Rarity.COMMON),
             new Tower("Iron Mine", new TowerStats(1, Resource.IRON, 3), 10, Rarity.COMMON)};
 
+    /**
+     * An Array of the 4 different types of {@link Upgrade}s available in the game.
+     *<p></p>
+     * Accessed by the ShopManager when generating new upgrades in the shop for the players.
+     * Their rarities are modified when generated.
+     *
+     * @see ShopManager
+     */
     final private Upgrade[] defaultUpgrades = new Upgrade[]{
             new Upgrade(new ExpBoost(10), Rarity.COMMON, 10, 1),
             new Upgrade(new CooldownReduction(1), Rarity.COMMON, 10, 1),
@@ -84,57 +122,154 @@ public class GameManager {
         gameGUIManager.launchScreen("Game Start");
     }
 
+    /**
+     * The constructor for {@link GameManager}.
+     * <p></p>
+     * Takes in a newly generated GameData and InventoryManager as a parameter and stores
+     * their value inside the GameManager.
+     * @param gameData {@link GameData}
+     * @param inventoryManager {@link InventoryManager}
+     */
     public GameManager(GameData gameData, InventoryManager inventoryManager){
         this.gameData = gameData;
         this.inventoryManager = inventoryManager;
     }
+
+    /**
+     * Getter for {@link GameManager#shopData}
+     * @return {@link ShopData}
+     */
     public ShopData getShopData(){return shopData;}
 
+    /**
+     * Takes in a {@link ShopData} as a parameter to set the
+     * GameManager's {@link GameManager#shopData} to
+     * @param shopData {@link ShopData}
+     */
+
     public void setShopData(ShopData shopData){this.shopData = shopData;}
+
+    /**
+     * Getter for the current {@link Round} of the game.
+     * @return {@link Round}
+     */
     public Round getRound(){return round;}
 
+    /**
+     * Takes in a parameter {@link Round} to set the current round to.
+     * @param round {@link Round}
+     */
     public void setRound(Round round){this.round = round;}
+
+    /**
+     * Getter for {@link GameManager#defaultTowers}
+     * @return {@link GameManager#defaultTowers}
+     */
     public Tower[] getDefaultTowers() {
         return defaultTowers;
     }
 
+    /**
+     * Getter for {@link GameManager#defaultUpgrades}
+     * @return {@link GameManager#defaultUpgrades}
+     */
     public Upgrade[] getDefaultUpgrades() {
         return defaultUpgrades;
     }
 
+    /**
+     * Getter for {@link GameManager#rarityData}
+     * @return {@link GameManager#rarityData}
+     */
     public RarityData getRarityData() { return rarityData;}
 
+    /**
+     * Getter for {@link GameManager#gameData}
+     * @return {@link GameManager#gameData}
+     */
     public GameData getGameData() {
         return gameData;
     }
 
+    /**
+     * Getter for {@link GameManager#inventoryManager}
+     * @return {@link GameManager#inventoryManager}
+     */
     public InventoryManager getInventoryManager() {
         return inventoryManager;
     }
 
+    /**
+     * Getter for {@link GameManager#defaultResources}
+     * @return {@link GameManager#defaultResources}
+     */
     public Resource[] getDefaultResources() { return defaultResources;}
 
+    /**
+     * Getter for {@link GameManager#gameGUIManager}
+     * @return {@link GameManager#gameGUIManager}
+     */
     public GameGUIManager getGameGUIManager() {
         return this.gameGUIManager;
     }
 
+    /**
+     * Takes in a parameter {@link GameData} to set the current
+     * GameManager's gameData to.
+     * <p></p>
+     * Called by the GameSetupController to initialize the GameData for GameManager.
+     * @param gameData {@link GameData}
+     * @see seng201.team8.gui.GameSetupController
+     */
     public void setGameData(GameData gameData){
         this.gameData = gameData;
     }
 
+    /**
+     * Takes in a parameter {@link InventoryManager} to set the current
+     * GameManager's inventoryManager to.
+     * <p></p>
+     * Called by the GameSetupController to initialize the InventoryManager for GameManager.
+     * @param inventoryManager {@link InventoryManager}
+     * @see seng201.team8.gui.GameSetupController
+     */
     public void setInventoryManager(InventoryManager inventoryManager) {
         this.inventoryManager = inventoryManager;
     }
 
+    /**
+     * Getter for {@link GameManager#roundWon}.
+     * <p></p>
+     * Returns true if the round is won and a false if the round is lost.
+     * @return {@link GameManager#roundWon}
+     */
     public boolean getRoundWon(){
         return roundWon;
     }
 
+    /**
+     * Setter to set the {@link GameManager#roundWon} value to the boolean parameter.
+     * <p></p>
+     * Used by the Round Evaluation Controller to signal that a round is won or lost, allowing
+     * other methods to be called.
+     * @see seng201.team8.gui.RoundEvaluationController
+     * @param roundWon {@link Boolean}
+     */
     public void setRoundWon(boolean roundWon) {
         this.roundWon = roundWon;
     }
+
+    /**
+     * Setter for {@link GameManager#roundResourceDisplay}, sets it to the inputted ArrayList
+     * @param resourceDisplay {@link ArrayList<Resource>}
+     */
     public void setRoundResourceDisplay(ArrayList<Resource> resourceDisplay){
         this.roundResourceDisplay = resourceDisplay;
     }
+
+    /**
+     * Getter for {@link GameManager#roundResourceDisplay}
+     * @return {@link GameManager#roundResourceDisplay}
+     */
     public ArrayList<Resource> getRoundResourceDisplay(){return roundResourceDisplay;}
 }
